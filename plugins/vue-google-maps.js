98f6b329c92ad3848cd41a65285f261a1ee6890e
cediatienza0725 <mac@admintest.local>
2024-11-14 12:18:49 -0500

    remove api key

--- plugins/vue-google-maps.js
+++ plugins/vue-google-maps.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import * as VueGoogleMaps from 'vue2-google-maps'
+import Vue from "vue";
+import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyC4ARSwu-AOLCGSKFi5AaqYhb5isnINxk0',
-    libraries: 'places',
+    key: "",
+    libraries: "places",
   },
-})
+});
